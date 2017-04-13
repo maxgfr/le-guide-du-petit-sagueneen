1c88f4ddffff46edee549e46aac656b0d98d7fac
maxgfr <maxgolfier@gmail.com>
2017-04-13 14:27:01 -0400

    :hamster:

--- app/src/main/java/com/example/etudiant/myapplication/InteretActivity.java
+++ app/src/main/java/com/example/etudiant/myapplication/InteretActivity.java
@@ -1,5 +1,13 @@
 package com.example.etudiant.myapplication;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -7,12 +15,50 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
 public class InteretActivity extends FragmentActivity implements OnMapReadyCallback {
 
+
+    // Distance de rafraichissement
+    private static final long LOCATION_REFRESH_DISTANCE = 1; // 10 meters
+    // Temps de rafraichissement
+    private static final long LOCATION_REFRESH_TIME = 1; // 1 minute
+
     private GoogleMap mMap;
+    private HashMap<LatLng, String> allPosition;
+
+
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +68,87 @@ public class InteretActivity extends FragmentActivity implements OnMapReadyCallb
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        allPosition = new HashMap<>();
     }
 
 
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    * Manipulates the map once available.
+    * This callback is triggered when the map is ready to be used.
+    * This is where we can add markers or lines, add listeners or move the camera. In this case,
+    * we just add a marker near Sydney, Australia.
+    * If Google Play services is not installed on the device, the user will be prompted to install
+    * it inside the SupportMapFragment. This method will only be triggered once the user has
+    * installed Google Play services and returned to the app.
+    */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    public void onMapReady(GoogleMap map) {
+        mMap = map;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //Ma Position
+        LatLng maPosition = getMyCurrentPosition();
+        //Zoom la Camera sur ma position
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(maPosition, 16));
+        //Ajoutons l'icone rouge
+        mMap.addMarker(new MarkerOptions()
+                .position(maPosition)
+                .draggable(true)
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+
+
+        //Initialisation des markers
+        initMarker();
+
+        for (Map.Entry<LatLng, String> entry : allPosition.entrySet()) {
+            mMap.addMarker(new MarkerOptions()
+                    .title(entry.getValue())
+                    .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+                    .position(entry.getKey()));
+        }
     }
+
+    private ArrayList<LocationProvider> getProviderGPS() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        ArrayList<LocationProvider> providers = new ArrayList<LocationProvider>();
+        ArrayList<String> names = (ArrayList<String>) locationManager.getProviders(true);
+
+        for (String name : names)
+            providers.add(locationManager.getProvider(name));
+
+        return providers;
+    }
+
+    private LatLng getMyCurrentPosition() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //not_possible
+        }
+        //pour update la position
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
+                LOCATION_REFRESH_DISTANCE, mLocationListener);
+        //recuperons notre position grâce au gps
+        Location localisation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        //transformant la en Latlng
+        LatLng maPos = new LatLng(localisation.getLatitude(), localisation.getLongitude());
+        return maPos;
+    }
+
+    public HashMap<LatLng,String> getListElements () {
+    return allPosition;
+    }
+
+    public void addPosition (LatLng p, String nom) {
+    allPosition.put(p,nom);
+    }
+
+    private void initMarker () {
+        LatLng lat = new LatLng(48.411509, -71.0156794);
+        allPosition.put(lat,"marker1");
+        LatLng lat2 = new LatLng(48.442509, -71.0226794);
+        allPosition.put(lat2,"marker2");
+        LatLng lat3 = new LatLng(48.498509, -71.0336794);
+        allPosition.put(lat3,"marker3");
+    }
+
 }
--- app/src/main/java/com/example/etudiant/myapplication/MainActivity.java
+++ app/src/main/java/com/example/etudiant/myapplication/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.etudiant.myapplication;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,8 +17,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    //Permission pour accéder au GPS
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 123 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,8 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +55,7 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         getSupportActionBar().setTitle("Le guide du petit sag");
+        requestPermission();
     }
 
 
@@ -107,5 +119,14 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), ACCESS_COARSE_LOCATION);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
     }
 }
